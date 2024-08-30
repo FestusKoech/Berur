4cc63600dc0fc11aa328179a2d25ceb1b3bf2300
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-08-30 19:55:48 +0300

    initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps:[
+
+        {
+            name: "inventory-management",
+            script: "npm",
+            args: "run-dev",
+            env: {
+                NODE_ENV: "development ",
+                ENV_VAR1: "enviroment-var",
+            }
+
+
+        }
+       
+
+    ]
+}
